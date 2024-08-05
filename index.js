4906d5a789a079acba3d4af55a2d451cc9f0d07e
Sam <sam.mjalaoui@gmail.com>
2024-08-05 14:47:36 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (!this.length){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length -1]
+  }
 
-  min() {}
+  min() {
+    if (!this.length){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((sum, item) => sum + item, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length){
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
